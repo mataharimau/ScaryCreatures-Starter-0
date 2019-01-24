c3c44779607775110da5951003747281eaa24a56
mataharimau <audrey.tam@gmail.com>
2019-01-25 09:09:32 +1100

    Initial Commit

---
+++ ScaryCreatures/Model/ScaryCreatureDoc.swift
@@ -0,0 +1,68 @@
+/// Copyright (c) 2018 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import UIKit
+
+class ScaryCreatureDoc: NSObject {
+  private var _data: ScaryCreatureData?
+  var data: ScaryCreatureData? {
+    get {
+      return _data
+    }
+    set {
+      _data = newValue
+    }
+  }
+  
+  private var _thumbImage: UIImage?
+  var thumbImage: UIImage? {
+    get {
+       return _thumbImage
+    }
+    set {
+      _thumbImage = newValue
+    }
+  }
+  
+  private var _fullImage: UIImage?
+  var fullImage: UIImage? {
+    get {
+       return _fullImage
+    }
+    set {
+      _fullImage = newValue
+    }
+  }
+ 
+  init(title: String, rating: Float, thumbImage: UIImage?, fullImage: UIImage?) {
+    super.init()
+    _data = ScaryCreatureData(title: title, rating: rating)
+    self.thumbImage = thumbImage
+    self.fullImage = fullImage
+  }
+}
